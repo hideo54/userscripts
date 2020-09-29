b64f46c707e0c8fd5e56500b118daa1f97ee38cb
hideo54 <hideo54@hideo54.com>
2020-09-29 11:27:51 +0900

    Fix an error when there's no snippet

--- reliable-google-search.user.js
+++ reliable-google-search.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reliable Google Search
 // @namespace    https://github.com/hideo54
-// @version      0.2
+// @version      0.2.1
 // @description  Google 検索結果から、指定されたドメインのページの表示を強調したり目立たなくしたりします。
 // @author       hideo54
 // @match        https://www.google.com/search?*
@@ -11,7 +11,9 @@
 // ==/UserScript==
 
 const a = document.querySelector('div.g.mnr-c.g-blk div.g a');
-a.href = a.href.replace(/#:~:text=\S*$/, '');
+if (a) {
+    a.href = a.href.replace(/#:~:text=\S*$/, '');
+}
 
 // Manually Updated
 const reliableDomains = [
