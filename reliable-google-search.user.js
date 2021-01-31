1bdc4c7c23f3284c55f4b81425fe283ecf1b37a3
hideo54 <hideo54@hideo54.com>
2021-02-01 00:44:16 +0900

    Add kasu site

--- reliable-google-search.user.js
+++ reliable-google-search.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reliable Google Search
 // @namespace    https://github.com/hideo54
-// @version      0.3.0
+// @version      0.3.1
 // @description  Google 検索結果から、指定されたドメインのページの表示を強調したり目立たなくしたりします。
 // @author       hideo54
 // @match        https://www.google.com/search?*
@@ -50,6 +50,7 @@ const deprecatedDomains = [
     'xperimentalhamid.com',
     'www.it-swarm.dev',
     'www.it-swarm-ja.tech',
+    'www.it-swarm.jp.net',
     'qastack.jp',
     'ja.it-reply.net',
     'www.sejuku.net',
