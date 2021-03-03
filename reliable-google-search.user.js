cf1b2a93a654fc0999dfb5090918a75b0668e109
hideo54 <hideo54@hideo54.com>
2021-03-04 01:51:48 +0900

    Version update

--- reliable-google-search.user.js
+++ reliable-google-search.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reliable Google Search
 // @namespace    https://github.com/hideo54
-// @version      0.3.2
+// @version      0.3.3
 // @description  Google 検索結果から、指定されたドメインのページの表示を強調したり目立たなくしたりします。
 // @author       hideo54
 // @match        https://www.google.com/search?*
