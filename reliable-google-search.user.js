33bb7d7c2e7fceb8b262d614c358001192533686
Hideo Yasumoto <hideo54@hideo54.com>
2021-12-17 16:09:45 +0900

    Now I admit Qiita is not kasu site. Congrats!

--- reliable-google-search.user.js
+++ reliable-google-search.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Reliable Google Search
 // @namespace    https://github.com/hideo54
-// @version      1.9.3
+// @version      1.9.4
 // @description  Google 検索結果から、指定されたドメインのページの表示を強調したり目立たなくしたりします。
 // @author       hideo54
 // @match        https://www.google.com/search?*
@@ -68,7 +68,6 @@ const deprecatedDomains = [
     'mongoing.com',
     'python5.com',
     'qastack.jp',
-    'qiita.com',
     'stackovernet.xyz',
     'stackoverrun.com',
     'steakrecords.com',
